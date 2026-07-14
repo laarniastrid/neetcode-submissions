7f8dac4237447114015e1bd67d7d56d31e0c87cc
Astrid Sutton <11416752+laarniastrid@users.noreply.github.com>
2026-07-14 14:59:10 -0500

    Add: top-k-elements-in-list - submission-29

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-29.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const frequency: Record<number, number> = {};
+        const numBucket = Array.from({ length: nums.length + 1 }, () => []);
+
+        nums.forEach(num => {
+            frequency[num] ??= 0;
+            frequency[num]++;
+        });
+
+        for (const key in frequency) {
+            numBucket[frequency[key]].push(Number(key));
+        }
+
+        // const response = numBucket.filter(x => x.length > 0).reverse().flat().slice(0, k);
+        const response = numBucket.filter(x => x.length > 0).flat().reverse().slice(0, k);
+
+        console.log('response ', response);
+
+        return response;
+    }
+}
