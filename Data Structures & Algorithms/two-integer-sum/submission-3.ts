fedf084efd373eb287f8c20ee890508d77657791
Astrid Sutton <11416752+laarniastrid@users.noreply.github.com>
2026-07-14 13:32:57 -0500

    Add: two-integer-sum - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum/submission-3.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const seen = {};
+
+        for (let i = 0; i < nums.length; i++) {
+            const complement = target - nums[i];
+
+            if (seen[complement] !== undefined) {
+                const firstIndex = seen[complement];
+                
+                return [firstIndex, i];
+            }
+
+            seen[nums[i]] = i;
+        }
+
+        return [];
+    }
+}
